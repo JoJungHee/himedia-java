66a3eda9e9e6dc47f477311c5d2107dc9cba1df9
JoJungHee <gt8503@naver.com>
2025-08-29 19:04:13 +0900

    j250829 HashMapTest02 추가

--- HashMapTest02/src/HashMapTest02.java
+++ HashMapTest02/src/HashMapTest02.java
@@ -1,4 +1,7 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /*
 문제)
@@ -20,9 +23,63 @@ public class HashMapTest02 {
 		map.put("김준완", 39);
 		map.put("채송화", 45);
 		System.out.println(map);
+		System.out.println("크기: " + map.size());
 		
+		// 키 중복
+		System.out.println("키중복");
+		map.put("이익준", 33);
+		System.out.println(map);
+		System.out.println("크기: " + map.size());
+		
+		
+		// 값 중복
+		System.out.println("값중복");
+		map.put("추민하", 45);
+		System.out.println(map);
+		System.out.println("크기: " + map.size());
+		
+		// 데이터 수정
+		System.out.println("데이터 수정");
+		map.put("채송화", 30);
+		System.out.println(map);
+		System.out.println("크기: " + map.size());
+		
+		// 데이터 삭제
+		System.out.println("데이터 삭제");
 		map.remove("채송화");
 		System.out.println(map);
+		System.out.println("크기: " + map.size());
+		
+		// 출력 방법 1 - Iterator + keySet()
+		Set<String> keySet = map.keySet();
+		Iterator<String> it = keySet.iterator();
+		while (it.hasNext()) {
+			String name = it.next();
+			int age = map.get(name);
+			System.out.printf("이름: %s(%d)\n", name, age);
+			
+		}
+		System.out.println("---------");
+		
+		// 출력 방법 2 - Iterator + entrySet()
+		Set<Entry<String, Integer>> entrySet = map.entrySet();
+		Iterator<Entry<String, Integer>> entryIt = entrySet.iterator();
+		
+		while (entryIt.hasNext()) {
+			Entry<String, Integer> entry = entryIt.next();
+			String name = entry.getKey();
+			int age = entry.getValue();
+			System.out.printf("이름: %s(%d)\n", name, age);
+		}
+		System.out.println("---------");
+		
+		// 출력 방법 3 - for-in + entrySet()
+		Set<Entry<String, Integer>> entrySet2 = map.entrySet();
+		for (Entry<String, Integer> entry : entrySet2) {
+			String name = entry.getKey();
+			int age = entry.getValue();
+			System.out.printf("이름: %s(%d)\n", name, age);
+		}
 		
 	}
 }
